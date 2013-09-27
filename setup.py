6c53771c5146ae97942ccbfc7b0bc52c12eb73af
Alexander Boyd <alex@opengroove.org>
2013-09-27 01:35:58 -0600

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="stm",
-    version="0.2.1",
+    version="0.2.2",
     description="A software transactional memory library",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
