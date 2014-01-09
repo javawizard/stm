9a0a45bb26dffbe9afb0af1cd855008dabc60e5b
Alexander Boyd <alex@opengroove.org>
2014-01-09 02:59:37 -0700

    Document eventloop.schedule and catch all exceptions inside the event loop

--- stm/eventloop.py
+++ stm/eventloop.py
@@ -39,20 +39,28 @@ class EventLoop(object):
                 return
             try:
                 next_event()
-            except Exception:
+            except:
                 print "Event threw an exception, which will be ignored."
                 print "For reference, the exception is:"
                 traceback.print_exc()
-            except:
-                print "Event threw a non-standard exception:"
-                traceback.print_exc()
-                raise
 
 
 default_event_loop = stm.atomically(EventLoop)
 default_event_loop.start()
 
 def schedule(function):
+    """
+    Schedule a function to be run later.
+    
+    This can be used from within a transaction to schedule a function to be
+    called outside of the transaction, after it commits. The specified function
+    will be run outside of the context of a transaction, so it can do things
+    like I/O that transactions normally aren't allowed to do.
+    
+    Note that such functions are run synchronously and in the order they were
+    scheduled. They should therefore complete quickly, or spawn a new thread
+    (or make use of a stm.threadutils.ThreadPool) to do their work.
+    """
     default_event_loop.schedule(function)
 
 
