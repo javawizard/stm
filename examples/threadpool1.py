6364db38a04fd8b27dc31035c8d1febf24bbc43f
Alexander Boyd <alex@opengroove.org>
2014-01-20 23:12:06 -0700

    Make the threadpool example even more demanding

--- examples/threadpool1.py
+++ examples/threadpool1.py
@@ -6,7 +6,7 @@ import random
 import operator
 import time
 
-pool = ThreadPool(50, 1)
+pool = ThreadPool(200, 10)
 
 @atomically_watch(lambda: (pool.tasks_scheduled, pool.tasks_finished, pool._live_threads, pool._free_threads, len(pool._tasks)))
 @changes_only(according_to=operator.eq)
@@ -15,14 +15,14 @@ def _((scheduled, finished, live, free, tasks)):
     print "{0} scheduled, {1} finished, {2} threads live, {3} free, {4} tasks scheduled".format(scheduled, finished, live, free, tasks)
 
 def task():
-    time.sleep(random.random() + 0.5)
+    time.sleep(10 + random.random() * 5)
 
-for _ in range(100):
+for _ in range(400):
     pool.schedule(task)
 
 pool.join()
 
-time.sleep(3)
+time.sleep(15)
 
 # TODO: Figure out why ThreadPool's keep_alive isn't keeping threads
 # (and thus this script) alive for 3 seconds after we join...
