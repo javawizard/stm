5af7252505c80dec5315f97d11e8bf1cdd6aa0a0
Alexander Boyd <alex@opengroove.org>
2014-01-20 21:22:20 -0700

    Set the threadpool example's thread pool to use 50 threads with 100 tasks, a combination that performs quite poorly at present due to contention among the threads. This will serve as a good benchmark to see if the performance improvements I'm about to make to the core have any (and enough) effect.

--- examples/threadpool1.py
+++ examples/threadpool1.py
@@ -6,7 +6,7 @@ import random
 import operator
 import time
 
-pool = ThreadPool(5, 3)
+pool = ThreadPool(50, 1)
 
 @atomically_watch(lambda: (pool.tasks_scheduled, pool.tasks_finished, pool._live_threads, pool._free_threads, len(pool._tasks)))
 @changes_only(according_to=operator.eq)
@@ -17,10 +17,12 @@ def _((scheduled, finished, live, free, tasks)):
 def task():
     time.sleep(random.random() + 0.5)
 
-for _ in range(40):
+for _ in range(100):
     pool.schedule(task)
 
 pool.join()
+
+time.sleep(3)
 
 # TODO: Figure out why ThreadPool's keep_alive isn't keeping threads
 # (and thus this script) alive for 3 seconds after we join...
